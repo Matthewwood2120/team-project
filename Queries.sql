4ef0943aeead3088e3f6aab7a39b95a2c4813a0e
Dillon <dt56109n@pace.edu>
2025-05-02 14:06:51 -0400

    fdsf

--- Queries.sql
+++ Queries.sql
@@ -4,3 +4,6 @@ SELECT * FROM seasons WHERE att > 15000;
 
 SELECT team, wins, shoot_out_wins, ties, losses, shoot_out_losses, games_played FROM standings;
 
+SELECT player, games_played, goals, shots, assists FROM stats WHERE goals > 0;
+
+
