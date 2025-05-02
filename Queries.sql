26102677a3a2e051ecc908c8a8d8492324f2c1b8
Dillon <dt56109n@pace.edu>
2025-05-02 13:21:03 -0400

    dfsfgs

---
+++ Queries.sql
@@ -0,0 +1,6 @@
+SELECT * FROM games WHERE score IS NOT NULL;
+
+SELECT * FROM seasons WHERE att > 15000;
+
+SELECT team, wins, shoot_out_wins, ties, losses, shoot_out_losses, games_played FROM standings;
+
