ef5935dfcfed2da903811fc2a1ac8eeaabc311c3
Matthewwood2120 <mw70079p@pace.edu>
2025-05-04 20:57:19 -0400

    queries

--- Queries.sql
+++ Queries.sql
@@ -5,5 +5,3 @@ SELECT * FROM seasons WHERE att > 15000;
 SELECT team, wins, shoot_out_wins, ties, losses, shoot_out_losses, games_played FROM standings;
 
 SELECT player, games_played, goals, shots, assists FROM stats WHERE goals > 0;
-
-
